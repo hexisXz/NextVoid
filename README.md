bbeefdf2fd8fcb6d943e4e26af7504dbdd69ced5
hexis <hexis@void.lan>
2023-08-28 22:50:59 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # NextVoid
 NextVoid is a void linux rice that is fast and sorta minimal 
 
+# how to install 
 
+``` sh
+sh <(curl -v -L https://raw.githubusercontent.com/hexisXz/NextVoid/main/install.sh)
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 
+sudo xbps-install -Syu ; sudo xbps-install -S alsa-utils polybar alacritty btop bat exa ksh picom rofi xwallpaper xtools && xcheckrestart && curl -L -O "https://raw.githubusercontent.com/hexisXz/setwall/main/install.sh" && bash install.sh && sh <(curl -v -L https://raw.githubusercontent.com/hexisXz/ebps/main/install) && git clone https://github.com/hexisXz/NextVoid.git && mv NextVoid/herbstluftwm ~/.config/ && mv NextVoid/void/.kshrc ~/ && echo "ksh" >> ~/.bashrc && mv NextVoid/void/.vimrc ~/ && mkdir ~/.config/alacritty && mv NextVoid/void/alacritty.yml ~/.config/alacritty && mkdir ~/.config/polybar/ && mv NextVoid/void/config.ini ~/.config/polybar && mv NextVoid/void/picom.conf ~/.config && mkdir ~/stuff && mv NextVoid/void/rofi-power-menu ~/stuff && mv NextVoid/void/rofi-wifi-menu ~/stuff 
 
